a56eda90415c825d413e7c49e32041b207e9babc
BrendhaCruz <brendha.cruz@gmail.com>
2017-11-27 11:47:23 -0300

    voltando ao inicio openshift

--- server.js
+++ server.js
@@ -2,71 +2,49 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-var fs       = require('fs');
-var mongo    = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
-var url 	   = require('url');
-var mdb      = require('./models/mongo');
+//var mongo = require('mongodb');
+//var ObjectId = require('mongodb').ObjectID;
 
-Object.assign=require('object-assign');
+Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-port = 3000 || process.env.PORT;
-mongoURL = 'mongodb://brunabarbosa:brunaolib13@ds147681.mlab.com:47681/todo_app_brunabarbosa';
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+        mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+        mongoUser = process.env[mongoServiceName + '_USER'];
 
-// console.log("####################################################");
-// console.log(">>> ", mongoURL);
-// console.log(">>> ", process.env.DATABASE_SERVICE_NAME);
-// console.log(">>> ", (mongoURL === null || mongoURL === undefined) && process.env.DATABASE_SERVICE_NAME === undefined);
-
-if ((mongoURL === null || mongoURL === undefined)) {
-    console.log("####################################################");
-    console.log(">>> ", mongoURL);
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME !== null ? process.env.DATABASE_SERVICE_NAME : "",
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'] ||"127.0.0.1",
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'] || "27017",
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'] || "teste",
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'] || "",
-    mongoUser = process.env[mongoServiceName + '_USER'] || "";
-
-    console.log(`>>> , host: ${mongoHost} port: ${mongoPort} database: ${mongoDatabase}`);
     if (mongoHost && mongoPort && mongoDatabase) {
         mongoURLLabel = mongoURL = 'mongodb://';
-
         if (mongoUser && mongoPassword) {
-        mongoURL += mongoUser + ':' + mongoPassword + '@';
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
         }
         // Provide UI label that excludes user id and pw
         mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
         mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
     }
 }
-
-
-/*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-// mdb.init(function (err) {
-//     if (err) {
-//         throw err;
-//     }
-// });
 
 var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
-    if (mongoURL === null) return;
-    console.log("####################################################");
-    console.log(mongoURL);
-    mongo.connect(mongoURL, function(err, conn) {
+    if (mongoURL == null) return;
+
+    var mongodb = require('mongodb');
+    if (mongodb == null) return;
+
+    mongodb.connect(mongoURL, function(err, conn) {
         if (err) {
             callback(err);
             return;
@@ -79,16 +57,6 @@ var initDb = function(callback) {
 
         console.log('Connected to MongoDB at: %s', mongoURL);
     });
-
-        // Custom modules
-    app.use('/user',    require('./models/user'));
-    app.use('/poetry',  require('./models/poetry'));
-    app.use('/comment', require('./models/comment'))
-    app.use('/like',    require('./models/like'));
-    app.use('/notif',   require('./models/notif'));
-    app.use('/follow',  require('./models/follow'));
-    app.use('/share',  require('./models/share'));
-
 };
 
 app.get('/', function (req, res) {
@@ -123,7 +91,6 @@ app.get('/pagecount', function (req, res) {
         res.send('{ pageCount: -1 }');
     }
 });
-
 
 // error handling
 app.use(function(err, req, res, next){
